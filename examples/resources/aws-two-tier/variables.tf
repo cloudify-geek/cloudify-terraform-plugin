94cf62c86d0fb92a8b553218ebd517c8c4a691d9
Ahmad Musa <ahmad@cloudify.co>
2020-02-13 13:48:58 +0200

    Fixes

--- examples/resources/aws-two-tier/variables.tf
+++ examples/resources/aws-two-tier/variables.tf
@@ -12,12 +12,12 @@ variable "aws_region" {
   description = "AWS region to launch servers."
 }
 
-# Ubuntu Precise 12.04 LTS (x64)
+# CentOS 7.7.1908 x86_64 with cloud-init (HVM)
 variable "aws_amis" {
   default = {
-    eu-west-1 = "ami-38708b45"
-    us-east-1 = "ami-ee6f5e8b"
-    us-west-1 = "ami-fb32279b"
-    us-west-2 = "ami-a523b4dd"
+    eu-west-1 = "ami-0eee6eb870dc1cefa"
+    us-east-1 = "ami-03248a0341eadb1f1"
+    us-west-1 = "ami-01dd5a8ef26e6341d"
+    us-west-2 = "ami-024b56adf74074ca6"
   }
 }
